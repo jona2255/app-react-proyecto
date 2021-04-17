3bce19554032588e0b0b32937c640b3856bb36d3
Jonatan <Snchez>
2021-04-17 16:20:57 +0200

    Muestra el listado de ODS en el home, que lo recibe de la API

--- src/components/OdsList.js
+++ src/components/OdsList.js
@@ -1,20 +1,22 @@
+import { useEffect } from "react";
 import { Card, CardColumns } from "react-bootstrap";
+import { useFetch } from "../hooks/useFetch";
 
 const ListOds = () => {
 
-  const listOds = [{
-    imagen: "imagen",
-    nombre: "nombre",
-    descripcion: "descripcion"
-  }];
+  const { datos, pideDatos } = useFetch();
+  useEffect(() => {
+    pideDatos(`${process.env.REACT_APP_API}ods`);
+  }, [pideDatos]);
 
   return (
     <>
       <h2 className="mb-5">Listado de ODS</h2>
-      {
-        listOds.map(ods => (
-          <CardColumns>
-            <Card>
+      <CardColumns>
+        {
+          datos
+          && datos.datos.map(ods => (
+            <Card key={ods._id}>
               <Card.Img variant="top" src={ods.imagen} />
               <Card.Body>
                 <Card.Title>{ods.imagen}</Card.Title>
@@ -23,9 +25,9 @@ const ListOds = () => {
                 </Card.Text>
               </Card.Body>
             </Card>
-          </CardColumns>
-        ))
-      }
+          ))
+        }
+      </CardColumns>
     </>
   );
 };
