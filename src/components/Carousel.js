f1720072c5a489cb1a8767156a5ee938b9086e19
Jonatan <Snchez>
2021-04-17 17:23:45 +0200

    Maquetación del Home.

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -5,36 +5,26 @@ const CarouselHeader = () => {
     <Carousel fade interval={5000}>
       <Carousel.Item  >
         <img
-          src="https://mdbootstrap.com/img/Photos/Others/images/76.jpg"
-          alt="First slide"
-          className="d-block w-100 csel"
+          src="/img/carousel/carousel-1.jpg"
+          alt="Colegio en África"
+          className="d-block w-100 mask"
         />
-        <Carousel.Caption >
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://mdbootstrap.com/img/Photos/Others/images/76.jpg"
-          alt="Second slide"
+          src="/img/carousel/carousel-2.png"
+          alt="Favelas en Brasil"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}
+
         />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://mdbootstrap.com/img/Photos/Others/images/76.jpg"
-          alt="Third slide"
+          src="/img/carousel/carousel-3.jpg"
+          alt="Estepa en Mongolia"
         />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   )
