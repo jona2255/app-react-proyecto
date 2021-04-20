13dd4b07b2958bba9bf13a1b6c308166be1dbd77
Jonatan <Snchez>
2021-04-20 13:49:56 +0200

    Imágen de pruebas en los ods y ongs,  Maquetación básica de las ong.

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,24 +1,33 @@
-import { Card } from "react-bootstrap";
-import PropTypes from "prop-types";
+import { Card, Col, Row } from "react-bootstrap";
 
 const ItemList = props => {
   const { ongs } = props;
   const { logo, nombre, direccion, telefono, correo } = ongs;
   return (
-    <Card>
-      <Card.Img variant="top" src={logo} />
-      <Card.Body>
-        <Card.Title>{nombre}</Card.Title>
-        <Card.Text>
-          {direccion}
-        </Card.Text>
-        <Card.Text>
-          {telefono}
-        </Card.Text>
-        <Card.Text>
-          {correo}
-        </Card.Text>
-      </Card.Body>
+    <Card className="mt-3 mb-3 pt-3 pb-3">
+      <Row>
+        <Col md={3}>
+          <Card.Img
+            variant="top"
+            src="https://firebasestorage.googleapis.com/v0/b/ongeitor.appspot.com/o/ods%2Ffin_pobreza.svg?alt=media"
+            width="180" height="180"
+          />
+        </Col>
+        <Col md={9}>
+          <Card.Body>
+            <Card.Title>{nombre}</Card.Title>
+            <Card.Text>
+              {direccion}
+            </Card.Text>
+            <Card.Text>
+              {telefono}
+            </Card.Text>
+            <Card.Text>
+              {correo}
+            </Card.Text>
+          </Card.Body>
+        </Col>
+      </Row>
     </Card>
   );
 };
